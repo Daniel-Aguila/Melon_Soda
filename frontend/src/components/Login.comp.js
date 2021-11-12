6a2097002ff8e8d1933aff525b74915e72e37909
Daniel-Aguila <danielbaguila@gmail.com>
2021-11-11 20:35:28 -0600

    Added validation, and fixed minor issues with Login UI

--- frontend/src/components/Login.comp.js
+++ frontend/src/components/Login.comp.js
@@ -1,7 +1,10 @@
-import React from 'react'
-import {Form, Container, Row, Col, Button} from 'react-bootstrap'
+import React from 'react';
+import {Form, Container, Row, Col, Button} from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
-export const LoginForm = () =>{
+export const LoginForm = ({handleOnChange, email, pass, handleOnSubmit}) =>{
+    //We pass the handleOnChange function that was defined on Entry.page.js
+    //This way it is called everytime the input for email/password is changed hence 'onChange'
     return(
         <div>
             <Container>
@@ -9,14 +12,14 @@ export const LoginForm = () =>{
                     <Col>
                         <h1 className="text-center">Client Login</h1>
                         <hr/>
-                        <Form>
-                            <Form.Group className="text-md-right">
-                                <Form.Label column sm="5">Email</Form.Label>
-                                <Form.Control type="email" name="email" placeholder="Enter Email" required/>
+                        <Form autoComplete="off" onSubmit={handleOnSubmit}>
+                            <Form.Group>
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" name="email" onChange={handleOnChange} value={email} placeholder="Enter Email" required/>
                             </Form.Group>
-                            <Form.Group className="text-md-right">
-                                <Form.Label column sm="7">Password</Form.Label>
-                                <Form.Control type="password" name="password" placeholder="Enter Password" required/>
+                            <Form.Group>
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" name="password" onChange={handleOnChange} value={pass} placeholder="Enter Password" required/>
                             </Form.Group>
 
                             <Button type="submit">Login</Button>
@@ -26,4 +29,14 @@ export const LoginForm = () =>{
             </Container>
         </div>
     )
+};
+
+//We are using proptypes to verify what we are passing through components
+//This verifies that we are receiving a function for handleOnChange, a string for email, and pass
+// eslint-disable-next-line react/no-typos
+LoginForm.propTypes = {
+    handleOnChange: PropTypes.func.isRequired,
+    handleOnSubmit: PropTypes.func.isRequired,
+    email: PropTypes.string.isRequired,
+    pass: PropTypes.string.isRequired,
 };
--- frontend/src/pages/Entry.page.js
+++ frontend/src/pages/Entry.page.js
@@ -1,14 +1,50 @@
-import React from 'react'
-import './Entry.style.css'
-import '../components/Login.comp'
+import React, {useState} from 'react';
+import './Entry.style.css';
+import '../components/Login.comp';
 import { LoginForm } from '../components/Login.comp';
 
 export const Entry = () =>{
+    //useState allows us to sate components
+    //const [state, setstate] = useState('')
+    //state is the variable and setstate is the function to update the variable state
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+const handleOnChange = event =>{
+    const {name, value} = event.target
+
+    switch(name){
+        //Everytime it is an email/password, the value is set to its designated variable
+        //using the setState function
+        //for example, setEmail(value) where value = 'value_' will set the email variable to 'variable_'
+        case 'email':
+            setEmail(value)
+            break
+        case 'password':
+            setPassword(value)
+            break
+            default:
+                break
+    }
+    console.log(name, value);
+};
+
+const handleOnSubmit = event =>{
+    //event.preventDefault() prevents the default behavior so we can do our own behavior
+    event.preventDefault();
+    //=====================================================================================================//
+    //If you want to do more validation do it here
+    if(!email || !password){
+        alert("Melon Soda demands you fill all the necessary information")
+    }
+    //TODO Call the api to submit the form
+};
+    //We pass the handleOnChange function to the Login.comp.js file
     return (
         <div className="entry-page">
             <div className="jumbotron">
-            <LoginForm/>
+            <LoginForm handleOnChange={handleOnChange} email={email} pass={password} handleOnSubmit={handleOnSubmit}/>
             </div>
         </div>
-    )
+    );
 };
