106fd784b6a6bb59924ca619ccad6243b98b9e0b
Daniel-Aguila <danielbaguila@gmail.com>
2021-11-12 02:52:01 -0600

    ....

--- frontend/src/pages/AddTicket.page.js
+++ frontend/src/pages/AddTicket.page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Container,Row,Col, Form} from 'react-bootstrap'
+import {Container,Row,Col,Form} from 'react-bootstrap'
 
 export const AddTicket = () => {
     return (
@@ -7,4 +7,4 @@ export const AddTicket = () => {
             ADD FORM
         </Container>
     )
-}
+};
