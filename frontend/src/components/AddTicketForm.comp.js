0197d1c90b3becec168d3ff6414a6b0d329e39c7
Daniel-Aguila <danielbaguila@gmail.com>
2021-11-12 20:25:49 -0600

    Finished Submit Ticket UI

--- frontend/src/components/AddTicketForm.comp.js
+++ frontend/src/components/AddTicketForm.comp.js
@@ -1,23 +1,41 @@
 import React from "react";
-import {Form, Container, Row, Col, Button} from "react-bootstrap";
+import {Form,Button} from "react-bootstrap";
+import PropTypes from 'prop-types';
+import '../pages/AddTicket.style.css'
 
-export const AddTicketForm = () => {
+export const AddTicketForm = ({handleOnSubmit, handleOnChange, subject, date, description}) => {
     return(
         <div>
             <h1 className="text-center">New Ticket</h1>
             <hr/>
-            <Form autoComplete="off">
+            <div className="jumbotron">
+            <Form autoComplete="off" onSubmit={handleOnSubmit}>
                 <Form.Group>
                     <Form.Label>Subject</Form.Label>
-                    <Form.Control name="subject"  placeholder="Enter Email" required/>
+                    <Form.Control name="subject"  onChange={handleOnChange} value={subject} placeholder="Enter Subject" required/>
                 </Form.Group>
                 <Form.Group>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name="password"  placeholder="Enter Password" required/>
+                    <Form.Label>Date</Form.Label>
+                    <Form.Control type="date" name="date" onChange={handleOnChange} value={date} placeholder="dd/mm/yyyy" required/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control name="description" onChange={handleOnChange} value={description} as="textarea" rows="5" cols="80" placeholder="Describe the issue at hand" required/>
                 </Form.Group>
                 <hr/>
-                <Button type="submit">Login</Button>
+                <div className="text-center">
+                <Button type="submit" style={{'font-size':'2rem', padding:'10px 30px'}}>Submit New Ticket</Button>
+                </div>
             </Form>
+            </div>
         </div>
     )
+};
+
+AddTicketForm.propTypes = {
+    handleOnChange: PropTypes.func.isRequired,
+    handleOnSubmit: PropTypes.func.isRequired,
+    subject: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
 };
--- frontend/src/pages/AddTicket.page.js
+++ frontend/src/pages/AddTicket.page.js
@@ -1,16 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Container,Row,Col,Form} from 'react-bootstrap'
 import {AddTicketForm} from '../components/AddTicketForm.comp'
 import './AddTicket.style.css';
 
+
+
 export const AddTicket = () => {
+
+    //useState allows us to sate components
+    //const [state, setstate] = useState('')
+    //state is the variable and setstate is the function to update the variable state
+    const [subject, setSubject] = useState('');
+    const [date, setDate] = useState('');
+    const [description, setDescription] = useState('');
+
+const handleOnChange = event =>{
+    const {name, value} = event.target
+
+    switch(name){
+        case 'subject':
+            setSubject(value)
+            break
+        case 'date':
+            setDate(value)
+            break
+        case 'description':
+            setDescription(value)
+            break
+        default:
+            break
+    }
+    console.log(name, value);
+};
+
+const handleOnSubmit = event =>{
+    //event.preventDefault() prevents the default behavior so we can do our own behavior
+    event.preventDefault();
+    //=====================================================================================================//
+    //If you want to do more validation do it here
+    if(!subject || !date || !description){
+        alert("Melon Soda demands you fill all the necessary information")
+    }
+    //TODO Call the api to submit the form
+};
+
     return (
             <Container>
                 <Row>
                     <Col>
-                        <div className="Jumbotron">
-                            <AddTicketForm/>
-                        </div>
+                        <AddTicketForm handleOnChange={handleOnChange} subject={subject} date={date} description={description} handleOnSubmit={handleOnSubmit}/>
                     </Col>
                 </Row>
             </Container>
