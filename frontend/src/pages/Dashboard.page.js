63b24426f604ed9f684d24f9ddf52a3de740fed9
Daniel-Aguila <danielbaguila@gmail.com>
2021-11-13 23:30:22 -0600

    Welcome

--- frontend/src/pages/Dashboard.page.js
+++ frontend/src/pages/Dashboard.page.js
@@ -9,19 +9,12 @@ export const Dashboard = () => {
     return(
         <DefaultLayout>
         <Container>
-            <Row>
-                {/* mt is margin top, and mb is margin bottom */}
-                <Col className="text-center mt-5 mb-2">
-                    <Button style={{'font-size':'2rem', padding:'10px 30px'}}>I am a Button that does not do anything</Button>
-                </Col>
-            </Row>
+
             <Row>
                 {/* mt is margin top, and mb is margin bottom */}
             </Row>
-            <hr/>
             <div className="Home">
-            <h1>Inovation.</h1><Spinner animation="grow">
-            </Spinner>
+            <h1>Welcome to Melon Soda's CRM</h1>
             </div>
         </Container>
         </DefaultLayout>
